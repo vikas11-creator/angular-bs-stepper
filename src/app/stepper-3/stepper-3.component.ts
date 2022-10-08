0f27a059cd84e38a05bb944aab247145bcebabe3
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2022-10-08 21:24:42 +0530

    save button added

--- src/app/stepper-3/stepper-3.component.ts
+++ src/app/stepper-3/stepper-3.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stepper-3',
   templateUrl: './stepper-3.component.html',
-  styleUrls: ['./stepper-3.component.css']
+  styleUrls: ['./stepper-3.component.css'],
 })
 export class Stepper3Component implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  save() {
+    this.router.navigate(['']);
   }
-
 }
