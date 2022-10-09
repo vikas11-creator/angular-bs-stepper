a871a1c32746786cc0e69bd07594e954ee7ab655
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2022-10-09 19:35:44 +0530

    eye added in input type password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   formSubmitAttempt: boolean = false;
   loginForm: FormGroup;
   workflowId: number = 0;
-  constructor(
-    private _formBuilder: FormBuilder,
-    private router: Router
-  ) {}
+  eyeButton: boolean = false;
+  password: string = 'password';
+  iconShow: boolean = true;
+  constructor(private _formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit() {
     this.logForm();
@@ -23,11 +23,29 @@ export class LoginComponent implements OnInit {
   logForm() {
     this.loginForm = this._formBuilder.group({
       name: ['', Validators.required],
-      description: ['', Validators.required],
+      password: ['', Validators.required],
     });
   }
 
   get f() {
     return this.loginForm.controls;
+  }
+
+  onSubmit(formVal) {
+    this.formSubmitAttempt = true;
+    this.iconShow = false;
+    if (this.loginForm.invalid) {
+      return;
+    }
+  }
+
+  togglePassword() {
+    if (this.password === 'password') {
+      this.password = 'text';
+      this.eyeButton = true;
+    } else {
+      this.password = 'password';
+      this.eyeButton = false;
+    }
   }
 }
