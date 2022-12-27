7a180ae5a495237b879b892e0c413d7392433649
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2022-12-27 20:23:39 +0530

    2 files modified

--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormArray, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormArray,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 @Component({
   selector: 'app-form-array',
   templateUrl: './form-array.component.html',
-  styleUrls: ['./form-array.component.css']
+  styleUrls: ['./form-array.component.css'],
 })
 export class FormArrayComponent implements OnInit {
   // basicSettingsForm:any;
   // keyContacts:FormArray;
-  // constructor( private formbuilder : FormBuilder, 
+  // constructor( private formbuilder : FormBuilder,
   //   private router :Router,
   //   private route: ActivatedRoute,) { }
 
@@ -39,29 +45,57 @@ export class FormArrayComponent implements OnInit {
   // removeBtn(index: number){
   //   this.keyContacts?.removeAt(index)
   // }
-  survey: FormGroup;
-  myForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-
-  }
+  //survey!: FormGroup;
+  myForm!: FormGroup;
+  isChange: boolean = false;
+  isformStart: boolean = false;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.createForm();
   }
 
+  changeEvent(event: any) {
+    console.log(event.target.value);
+    let value = event.target.value;
+    // if(value){
+    this.isChange = true;
+    //this.createPeopleArray()
+    //this.createPeopleArray();
+    //}
+  }
+
+  callForm(type: any) {
+    console.log('ghgjj');
+    // console.log(formControlName)
+    if (type == 'male') {
+      this.isformStart = true;
+      this.createForm();
+    }
+  }
+
   createForm() {
     this.myForm = this.fb.group({
-      people: this.fb.array([this.createPeopleArray()])
-    })
+      filterField: ['', [Validators.required]],
+      // genderControl: ['', [Validators.required]],
+      people: this.fb.array([this.createPeopleArray()]),
+    });
+  }
+
+  createNew() {
+    return this.fb.group({
+      name: null,
+      addresses: new FormArray([this.createAddressArray()]),
+    });
   }
 
   createPeopleArray() {
+    // if(this.isChange){
     return this.fb.group({
       name: null,
-      addresses: new FormArray([
-        this.createAddressArray()
-      ])
+      addresses: new FormArray([this.createAddressArray()]),
     });
+    //}
   }
 
   getPeople(form) {
@@ -74,9 +108,19 @@ export class FormArrayComponent implements OnInit {
 
   createAddressArray() {
     return this.fb.group({
-      houseNo: null,
-      city: null
-    })
+      genderControl: [''],
+      Ys: this.fb.array([]),
+    });
+  }
+
+  initY() {
+    return this.fb.group({
+      //  ---------------------forms fields on y level ------------------------
+      Y1: ['Y1', [Validators.required, Validators.pattern('[0-9]{3}')]],
+      Y2: ['Y2', [Validators.required, Validators.pattern('[0-9]{3}')]],
+      // ---------------------------------------------------------------------
+      //Zs: this.fb.array([this.initZ()]),
+    });
   }
 
   addPeople() {
@@ -85,12 +129,15 @@ export class FormArrayComponent implements OnInit {
   }
 
   addAddress(i) {
-    const control = <FormArray>this.myForm.get('people').controls[i].get('addresses');
+    const control = <FormArray>(
+      this.myForm.get('people').controls[i].get('addresses')
+    );
     // console.log(control);
-    control.push(this.createAddressArray());
+    control.push(this.createNew());
+    console.log(this.myForm.get('people').controls[i]);
   }
 
-  submit() {
-    console.log(this.myForm.value)
+  submit(myForm) {
+    console.log(myForm);
   }
 }
