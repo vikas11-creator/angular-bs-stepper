271a0a2bc26629bae7b8fbfbdad7a90dcacca48b
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-05-03 17:37:40 +0530

    1 file modified

--- src/app/input-push/input-push.component.ts
+++ src/app/input-push/input-push.component.ts
@@ -26,6 +26,28 @@ export class InputPushComponent implements OnInit {
 
   pushData(obj) {
     console.log(obj);
+    let ind: number;
+    if (this.arr.length == 0) {
+      this.arr.push(obj);
+    } else {
+      let check = this.arr.some((el, i) => {
+        if (el.x == obj.x) {
+          ind = i;
+        }
+        return el.x == obj.x;
+      });
+      if (check) {
+        this.arr.splice(ind, 1, obj);
+      } else {
+        this.arr.push(obj);
+      }
+    }
+
+    let total = this.arr.reduce((acc, current) => {
+      return acc + current.v;
+    }, 0);
+
+    console.log(total);
   }
 
   onSubmit(formVal) {}
