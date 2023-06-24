8e1f451b41aa5a8932731f09386823a6ed551513
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-06-24 17:56:08 +0530

    1 file modified

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -16,7 +16,6 @@ export class EditorComponent implements OnInit {
   cols: any = [];
   colLength: any;
   isLoad: boolean = false;
-  selectedCells: any = [];
   indexList: any = [];
   localTableArray: any = [];
   constructor(
@@ -231,82 +230,84 @@ export class EditorComponent implements OnInit {
     });
   }
 
-  mergeColumns() {
-    if (this.validateMerge()) {
-      let mergeIndex = Number(Math.min(...this.indexList));
-      this.selectedCells.forEach((data: any) => {
-        this.tableRowControls['controls'].forEach((rowControl) => {
-          rowControl
-            .get('columns')
-            ['controls'].forEach((columnControl: any, i: any) => {
-              if (_.isEqual(columnControl.value, data)) {
-                if (mergeIndex != i) {
-                  _.remove(
-                    rowControl.get('columns')['controls'],
-                    (element: any) => _.isEqual(element.value, data)
-                  );
-                  _.remove(rowControl.get('columns')['value'], (element: any) =>
-                    _.isEqual(element, data)
-                  );
-                } else {
-                  this.setColSpan(columnControl, this.selectedCells);
-                }
-              }
-            });
-        });
-      });
-      this.selectedCells = [];
-      this.indexList = [];
-    } else {
-      this.indexList = [];
-      this.selectedCells.forEach((cell: any) => {
-        cell.checked = false;
-      });
-      this.selectedCells = [];
-      return;
-    }
-  }
 
-  setColSpan(cell, length) {
-    let sumWithInitial = this.selectedCells.reduce(
-      (accumulator, currentValue) => accumulator + currentValue.colspan,
-      0
-    );
-    if (cell.controls.colspan.value) {
-      cell.controls.colspan.value = sumWithInitial;
-    }
-    if (cell.value) {
-      cell.value.colspan = sumWithInitial;
-      cell.value.checked = false;
-    }
-  }
+  //subham code
+  // mergeColumns() {
+  //   if (this.validateMerge()) {
+  //     let mergeIndex = Number(Math.min(...this.indexList));
+  //     this.selectedCells.forEach((data: any) => {
+  //       this.tableRowControls['controls'].forEach((rowControl) => {
+  //         rowControl
+  //           .get('columns')
+  //           ['controls'].forEach((columnControl: any, i: any) => {
+  //             if (_.isEqual(columnControl.value, data)) {
+  //               if (mergeIndex != i) {
+  //                 _.remove(
+  //                   rowControl.get('columns')['controls'],
+  //                   (element: any) => _.isEqual(element.value, data)
+  //                 );
+  //                 _.remove(rowControl.get('columns')['value'], (element: any) =>
+  //                   _.isEqual(element, data)
+  //                 );
+  //               } else {
+  //                 this.setColSpan(columnControl, this.selectedCells);
+  //               }
+  //             }
+  //           });
+  //       });
+  //     });
+  //     this.selectedCells = [];
+  //     this.indexList = [];
+  //   } else {
+  //     this.indexList = [];
+  //     this.selectedCells.forEach((cell: any) => {
+  //       cell.checked = false;
+  //     });
+  //     this.selectedCells = [];
+  //     return;
+  //   }
+  // }
 
-  validateMerge() {
-    let flag: boolean = false;
-    let rowFlag: boolean = false;
-    let adjFlag: boolean = false;
-    let indexArr: any = [];
-    let adjacentArr: any = [];
-    //code for across rows
-    this.selectedCells.forEach((cell: any) => {
-      this.componentForm.value.rows.forEach((row: any, index: any) => {
-        if (row.columns.includes(cell)) {
-          indexArr.push(index);
-        }
-      });
-    });
-    this.componentForm.value.rows.forEach((row: any) => {
-      row.columns.forEach((column: any, index: number) => {
-        if (this.selectedCells.includes(column)) {
-          adjacentArr.push(index);
-        }
-      });
-    });
-    _.uniq(indexArr).length == 1 ? (rowFlag = true) : '';
-    adjFlag = this.sequntialIndices(adjacentArr, adjacentArr.length);
-    adjFlag == true && rowFlag == true ? (flag = true) : (flag = false);
-    return flag;
-  }
+  // setColSpan(cell, length) {
+  //   let sumWithInitial = this.selectedCells.reduce(
+  //     (accumulator, currentValue) => accumulator + currentValue.colspan,
+  //     0
+  //   );
+  //   if (cell.controls.colspan.value) {
+  //     cell.controls.colspan.value = sumWithInitial;
+  //   }
+  //   if (cell.value) {
+  //     cell.value.colspan = sumWithInitial;
+  //     cell.value.checked = false;
+  //   }
+  // }
+
+  // validateMerge() {
+  //   let flag: boolean = false;
+  //   let rowFlag: boolean = false;
+  //   let adjFlag: boolean = false;
+  //   let indexArr: any = [];
+  //   let adjacentArr: any = [];
+  //   //code for across rows
+  //   this.selectedCells.forEach((cell: any) => {
+  //     this.componentForm.value.rows.forEach((row: any, index: any) => {
+  //       if (row.columns.includes(cell)) {
+  //         indexArr.push(index);
+  //       }
+  //     });
+  //   });
+  //   this.componentForm.value.rows.forEach((row: any) => {
+  //     row.columns.forEach((column: any, index: number) => {
+  //       if (this.selectedCells.includes(column)) {
+  //         adjacentArr.push(index);
+  //       }
+  //     });
+  //   });
+  //   _.uniq(indexArr).length == 1 ? (rowFlag = true) : '';
+  //   adjFlag = this.sequntialIndices(adjacentArr, adjacentArr.length);
+  //   adjFlag == true && rowFlag == true ? (flag = true) : (flag = false);
+  //   return flag;
+  // }
 
   sequntialIndices(arr: any, n: any) {
     arr.sort();
